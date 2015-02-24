7c5804e063dc7b35d45659ecb8a068d142d72505
Dan Farrelly <daniel.j.farrelly@gmail.com>
2015-02-25 00:16:54 +1100

    Add gulp

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var rename = require('gulp-rename');
+var transform = require('vinyl-transform');
+var browserify = require('browserify');
+var reactify = require('reactify');
+
+gulp.task('browserify', function() {
+
+  var browserified = transform(function(filename) {
+    return browserify(filename)
+      .transform(reactify)
+      .bundle();
+  });
+
+  return gulp.src(['app/*.js'])
+    .pipe(browserified)
+    .pipe(gulp.dest('public/js/bundles'));
+
+});
+
+gulp.task('watch', function() {
+  return gulp.watch([
+    'app/**/*.js',
+    'app/**/*.jsx'
+  ], ['browserify']);
+});
+
+gulp.task('default', ['browserify']);
