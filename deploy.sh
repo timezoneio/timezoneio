6a8a4e80a0f44cc8b5a7daa0fb4c2c5b203231df
Dan Farrelly <daniel.j.farrelly@gmail.com>
2015-03-26 00:08:17 -0400

    New deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,4 @@
-jsonlint people.json -q
-LINT_RESULT=$?
-if [ $LINT_RESULT -ne 0 ] ; then
-    exit 1
-fi
-rsync -avz ./people.json root@timezone.io:/root/timezoneio/people.json
-ssh root@timezone.io "pm2 restart web"
+#!/bin/sh
+git checkout master && git pull
+npm install
+pm2 restart web
