9f81ced62ea99be884367ed4c7b85efc29ed7bc9
Dan Farrelly <daniel.j.farrelly@gmail.com>
2015-06-20 16:10:27 -0400

    Fix merge issue

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ connect();
 mongoose.connection.on('error', console.error);
 mongoose.connection.on('disconnected', connect);
 mongoose.connection.once('open', function (callback) {
-  
+
   console.info('We\'re connected, booyah! Starting up the server...');
   server();
 
@@ -22,36 +22,5 @@ mongoose.connection.once('open', function (callback) {
 // UserModal.findOne({ username: 'dan' }, function(err, user) {
 
 //   console.info( user.authenticate('password') ? 'OK!' : 'NO' );
-  
+
 // });
-
-  var body = React.renderToString(
-    React.createElement(App, {
-      time: time,
-      timezones: timezones,
-      timeFormat: timeFormat,
-      isCurrentTime: isCurrentTime
-    })
-  );
-
-  var params = {
-    title: strings.capFirst(req.params.name),
-    body: body,
-    script: 'bundles/app.js',
-    data: {
-      time: time,
-      people: people,
-      timeFormat: timeFormat,
-      isCurrentTime: isCurrentTime
-    }
-  };
-
-  render(req, res, params);
-
-});
-
-// Static files
-app.use(express.static(__dirname + '/public'));
-
-//process.env.PORT || 
-app.listen(8080);
