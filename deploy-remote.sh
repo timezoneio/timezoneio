2ed72fe610b39c9c05ffd3c6f4d9f58a41f6e9af
Dan Farrelly <daniel.j.farrelly@gmail.com>
2015-06-22 01:13:59 -0400

    Update deploy script

--- deploy-remote.sh
+++ deploy-remote.sh
@@ -1,8 +1,2 @@
 #!/bin/sh
-jsonlint people.json -q
-LINT_RESULT=$?
-if [ $LINT_RESULT -ne 0 ] ; then
-    exit 1
-fi
-rsync -avz ./people.json root@timezone.io:/root/timezoneio/people.json
 ssh root@timezone.io "cd /root/timezoneio && ./deploy.sh"
