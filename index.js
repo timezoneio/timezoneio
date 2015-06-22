8b4d11cd613ebe3fa1a0a8fdbf85e96155cbd2f1
Dan Farrelly <daniel.j.farrelly@gmail.com>
2015-06-22 00:59:31 -0400

    Test out Docker production setup

--- index.js
+++ index.js
@@ -2,9 +2,27 @@ var mongoose = require('mongoose');
 
 var server = require('./app/server.js');
 
+var MAX_RETRIES = 100;
+var retries = -1;
+
+var env = process.env.NODE_ENV;
+var isProduction = env === 'production';
+
+// In production 'db' is added to the hosts file during linking
+var auth = 'prod:[WexeYHJcqXjxA3;gT.8';
+// var dbHost = isProduction ?
+//               auth + '@' + process.env.DB_PORT_27017_TCP_ADDR :
+//               'localhost';
+var dbHost = 'localhost';
+
 var connect = function () {
+  if (retries >= MAX_RETRIES)
+    return console.info('Couldn\'t connect to the database');
+
   var options = { server: { socketOptions: { keepAlive: 1 } } };
-  mongoose.connect('mongodb://localhost/timezone', options);
+  mongoose.connect('mongodb://' + dbHost + '/timezone', options);
+
+  retries++;
 };
 connect();
 
