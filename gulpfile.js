2745611c245950d835c51a6bc1b171a386235376
Dan Farrelly <daniel.j.farrelly@gmail.com>
2015-09-13 20:10:29 -0400

    Add s3 upload workflow

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,10 @@ var transform = require('vinyl-transform');
 var browserify = require('browserify');
 var reactify = require('reactify');
 var uglify = require('gulp-uglify');
+var awspublish = require('gulp-awspublish');
+
+var awsCredentials = require('./aws.json');
+
 
 gulp.task('browserify', function() {
 
@@ -21,7 +25,59 @@ gulp.task('browserify', function() {
 
 });
 
-gulp.task('predeploy', function() {
+
+
+var getExpires = function() {
+  var expires = new Date();
+  expires.setFullYear(expires.getFullYear()+10);
+  return expires;
+};
+
+var s3Headers = {
+  'Cache-Control': 'max-age=315360000, no-transform, public',
+  'Expires': getExpires()
+};
+
+gulp.task('upload-images', function() {
+  var publisher = awspublish.create(awsCredentials);
+  return gulp.src(['public/images/**/*'])
+    .pipe(rename(function (path) {
+      path.dirname = 'images/' + path.dirname;
+    }))
+    .pipe(publisher.publish(s3Headers))
+    .pipe(publisher.cache())
+    .pipe(awspublish.reporter());
+});
+
+// TODO - Needs stylus compile + versioning
+gulp.task('upload-css', function() {
+  var publisher = awspublish.create(awsCredentials);
+  return gulp.src(['public/stylesheets/**/*'])
+    .pipe(rename(function (path) {
+      path.dirname = 'stylesheets/' + path.dirname;
+    }))
+    .pipe(awspublish.gzip({ ext: '.gz' }))
+    .pipe(publisher.publish(s3Headers))
+    .pipe(publisher.cache())
+    .pipe(awspublish.reporter());
+});
+
+// TODO - Needs versioning
+gulp.task('upload-js', function() {
+  var publisher = awspublish.create(awsCredentials);
+  return gulp.src(['public/js/bundles/*.js'])
+    .pipe(uglify())
+    .pipe(rename(function (path) {
+      path.dirname = 'js/bundles/' + path.dirname;
+    }))
+    .pipe(awspublish.gzip({ ext: '.gz' }))
+    .pipe(publisher.publish(s3Headers))
+    .pipe(publisher.cache())
+    .pipe(awspublish.reporter());
+});
+
+
+gulp.task('uglify', function() {
   return gulp.src(['public/js/bundles/*.js'])
     .pipe(uglify())
     .pipe(gulp.dest('public/js/bundles'));
@@ -33,5 +89,7 @@ gulp.task('watch', function() {
     'app/**/*.jsx'
   ], ['browserify']);
 });
+
+gulp.task('predeploy', ['upload-images'/*, 'upload-css'*/]);
 
 gulp.task('default', ['browserify']);
