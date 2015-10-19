00300725e238cd299af4c9ebd97dbb282f1aae2d
Dan Farrelly <daniel.j.farrelly@gmail.com>
2015-10-19 10:24:38 -0400

    Reorganized logic

--- app/helpers/getCityFromCoords.js
+++ app/helpers/getCityFromCoords.js
@@ -9,20 +9,23 @@ module.exports = function getCityFromCoords(lat, long, cb) {
     if (err) return cb(err);
     if (!data.results || !data.results.length) return cb('No results!');
 
-    // TODO - clean this up:
-    var city = null;
-    // var country = null;
-    // NOTE - city is "administrative_area_level_1"
-    for (var i = 0; i < data.results.length; i++) {
-      data.results[i].address_components.forEach(function(comp) {
-        if (!city && comp.types.indexOf('locality') >= 0)
-          city = comp.long_name;
-        // if (!country && comp.types.indexOf('country') >= 0) country = comp.long_name;
+    // NOTE - city is "administrative_area_level_1", town is "locality"
+    var cities = data.results
+      .map(function(result) {
+
+        // Find the component that is classified as a 'locality'
+        var cityComponent = result.address_components.filter(function(component) {
+          return component.types.indexOf('locality') >= 0;
+        })[0];
+
+        return cityComponent && cityComponent.long_name;
+      })
+      .filter(function(city) {
+        return  !!city;
       });
 
-      if (city)
-        return cb(null, city);
-    }
+    if (cities[0])
+      return cb(null, cities[0]);
 
     cb('No location found!');
   });
