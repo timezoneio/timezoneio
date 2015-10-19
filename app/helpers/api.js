5a9a6fdf0239a18ce7ec14fb39e8bb5a778e656c
Dan Farrelly <daniel.j.farrelly@gmail.com>
2015-10-19 11:54:23 -0400

    Only include fetch polyfill in client side bundle

--- app/helpers/api.js
+++ app/helpers/api.js
@@ -1,11 +1,9 @@
 // NOTE - must require fetch polyfill in app script
 if (typeof Promise === 'undefined') require('es6-promise').polyfill();
-require('./fetchPolyfill');
 var qs = require('querystring');
 
 // we rely on global csrf token for now, grab first so it can't be modified
 var CSRF = typeof window !== 'undefined' && window.appData.csrf_token;
-// var fetch = typeof window !== 'undefined' && window.fetch;
 
 // TOOD - Need promise polyfill
 var status = function(res) {
--- app/helpers/fetchPolyfill.js
+++ app/helpers/fetchPolyfill.js
@@ -1,6 +1,4 @@
 // Currently, the fetch API doesn't reliably parse the UTF-8 encoded json
 // correctly. Here we just force the polyfill
-if (typeof window !== 'undefined') {
-  window.fetch = void 0;
-  require('whatwg-fetch');
-}
+self.fetch = void 0;
+require('whatwg-fetch');
