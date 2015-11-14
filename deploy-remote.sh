4983ab3fca5733bc46bd9f0e23b203a7edd26c4e
Dan Farrelly <daniel.j.farrelly@gmail.com>
2015-11-14 13:01:19 -0500

    Update new server folder location. Experiment with staging conf

--- deploy-remote.sh
+++ deploy-remote.sh
@@ -4,7 +4,7 @@ read -p "Did you run the gulp predeploy script & commit it (y/n)? " answer
 case ${answer:0:1} in
     y|Y )
         echo "Awesome...time to deploy"
-        ssh root@timezone.io "cd /root/timezoneio && ./deploy.sh";
+        ssh root@timezone.io "cd /home/timezoneio && ./deploy.sh";
     ;;
     * )
         echo "Deploy aborted"
