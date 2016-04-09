a6a2b61e49c7a8eda9fbf4ceb521333d402c3860
Dan Farrelly <daniel.j.farrelly@gmail.com>
2016-04-10 06:50:31 +0800

    Fix get location flow w/ autocomplete

--- app/components/locationAutocomplete.jsx
+++ app/components/locationAutocomplete.jsx
@@ -19,7 +19,7 @@ module.exports = React.createClass({
 
   componentWillReceiveProps: function(nextProps) {
     // If this component gets it's location cleared or sent a fresh location we update
-    if (nextProps.clearLocation === true)
+    if (nextProps.clearLocation !== this.props.clearLocation)
       this.setState({ location: nextProps.location });
   },
 
