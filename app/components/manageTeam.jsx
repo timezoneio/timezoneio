a09cc6d45dc905a9e0aea67516f88a01d9b1c482
Dan Farrelly <daniel.j.farrelly@gmail.com>
2016-05-01 12:18:11 -0400

    Remove debug code

--- app/components/manageTeam.jsx
+++ app/components/manageTeam.jsx
@@ -199,10 +199,6 @@ var ManageTeam = React.createClass({
     var people = this.props.people;
     var visiblePeople = this.state.filter ? people.filter(this.peopleFilter) : people;
 
-    // DEBUG for #19
-    console.log(this.props.people.length);
-    console.log(JSON.stringify(this.state, null));
-
     var sortedPeople = visiblePeople.sort(userSort);
     var filterValueLink = {
       value: this.state.filterText,
