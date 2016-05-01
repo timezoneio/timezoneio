8006bcfb7dd051acbfdc0f14e5945786a4601a10
Dan Farrelly <daniel.j.farrelly@gmail.com>
2016-05-01 12:40:55 -0400

    Add name to team view

--- app/components/AppToolbar.jsx
+++ app/components/AppToolbar.jsx
@@ -20,34 +20,46 @@ class AppToolbar extends React.Component {
     return (
       <div className="app-toolbar">
 
-        <div className="app-toolbar-branding">
-          <Branding
-            link={true}
-            minimal={true}
-          />
+        <div className="app-toolbar-section app-toolbar-left">
+
+          <div className="app-toolbar-branding">
+            <Branding
+              link={true}
+              minimal={true}
+            />
+          </div>
+
+          <div className="app-toolbar-main">
+            <TimeControl {...this.props} />
+          </div>
+
         </div>
 
-        <div className="app-toolbar-main">
-          <TimeControl {...this.props} />
+        <div className="app-toolbar-section app-toolbar-center">
+          <h2 className="app-toolbar-name">
+            {this.props.team.name}
+          </h2>
         </div>
 
-        <div className="app-toolbar-actions">
-          <TeamSearch people={this.props.people} />
-          { this.props.isAdmin && (
-            <button className="manage-team-button material-icons md-18 clear"
-                    title="Manage your team"
-                    onClick={this.handleClickManage}>
-              settings
-            </button>
-          )}
-          {
-            this.props.user ?
-            <UserMenu {...this.props.user} /> :
-            <a href="/signup" className="button cta">
-              Sign up
-            </a>
-          }
+        <div className="app-toolbar-section app-toolbar-right">
+          <div className="app-toolbar-actions">
+            <TeamSearch people={this.props.people} />
+            { this.props.isAdmin && (
+              <button className="manage-team-button material-icons md-18 clear"
+                      title="Manage your team"
+                      onClick={this.handleClickManage}>
+                settings
+              </button>
+            )}
+            {
+              this.props.user ?
+              <UserMenu {...this.props.user} /> :
+              <a href="/signup" className="button cta">
+                Sign up
+              </a>
+            }
 
+          </div>
         </div>
 
       </div>
