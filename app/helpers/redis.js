56466e64797be5985aacf8709779f981beb89123
Dan Farrelly <daniel.j.farrelly@gmail.com>
2018-06-11 21:34:52 -0400

    Use redis url env variable

--- app/helpers/redis.js
+++ app/helpers/redis.js
@@ -1,7 +1,7 @@
 var redis = require('redis');
 
 var client = redis.createClient({
-  host: process.env.NODE_ENV === 'production' ? '127.0.0.1' : 'redis',
+  host: process.env.REDIS_URL || 'redis',
 });
 
 // This is a simple wrapper so we can re-use the active redis connection
