82ee9179be3dc2a4f8f6df1bde9ced29f984c52c
Dan Farrelly <daniel.j.farrelly@gmail.com>
2022-12-12 10:28:11 -0500

    Dummy commit

--- app/server.js
+++ app/server.js
@@ -1,27 +1,25 @@
-var path = require('path');
-var express = require('express');
-var logger = require('morgan');
-var slashes = require('connect-slashes');
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
-var RedisStore = require('connect-redis')(session);
-var bodyParser = require('body-parser');
-var csrf = require('csurf');
-var multer = require('multer');
-var flash = require('connect-flash');
-var passport = require('passport');
+var path = require("path");
+var express = require("express");
+var logger = require("morgan");
+var slashes = require("connect-slashes");
+var cookieParser = require("cookie-parser");
+var session = require("express-session");
+var RedisStore = require("connect-redis")(session);
+var bodyParser = require("body-parser");
+var csrf = require("csurf");
+var multer = require("multer");
+var flash = require("connect-flash");
+var passport = require("passport");
 
+require("node-jsx").install({ extension: ".jsx" });
 
-require('node-jsx').install({extension: '.jsx'});
+var stylusMiddleware = require("../config/middleware/stylus.js");
+var render = require("./helpers/render.js");
 
-var stylusMiddleware = require('../config/middleware/stylus.js');
-var render = require('./helpers/render.js');
+const isProduction = process.env.NODE_ENV === "production";
 
-const isProduction = process.env.NODE_ENV === 'production';
-
-module.exports = function(mongooseConnection, redisClient) {
-
-  require('../config/passport.js')(passport);
+module.exports = function (mongooseConnection, redisClient) {
+  require("../config/passport.js")(passport);
 
   var app = express();
 
@@ -33,27 +31,29 @@ module.exports = function(mongooseConnection, redisClient) {
   }
 
   app.use(slashes(false));
-  app.use(logger('dev'));
-  app.use(express.static(path.join(__dirname, '../public')));
+  app.use(logger("dev"));
+  app.use(express.static(path.join(__dirname, "../public")));
 
-  app.engine('jsx', render);
-  app.set('views', path.join(__dirname, 'views'));
-  app.set('view engine', 'jsx');
+  app.engine("jsx", render);
+  app.set("views", path.join(__dirname, "views"));
+  app.set("view engine", "jsx");
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(multer());
 
   app.use(cookieParser());
-  app.use(session({
-    resave: false, // don't save session if unmodified
-    saveUninitialized: false, // don't create session until something stored
-    secret: 'bodhi',
-    store: new RedisStore({
-      client: redisClient,
-      ttl: 14 * 86400 // 14 days expiration
+  app.use(
+    session({
+      resave: false, // don't save session if unmodified
+      saveUninitialized: false, // don't create session until something stored
+      secret: "bodhi",
+      store: new RedisStore({
+        client: redisClient,
+        ttl: 14 * 86400, // 14 days expiration
+      }),
     })
-  }));
+  );
 
   app.use(passport.initialize());
   app.use(passport.session());
@@ -63,8 +63,8 @@ module.exports = function(mongooseConnection, redisClient) {
 
   // Don't need CSRF w/ access tokens
   var csrfMiddleware = csrf();
-  app.use(function(req, res, next) {
-    var isAPI = req.originalUrl.slice(0, 4) === '/api';
+  app.use(function (req, res, next) {
+    var isAPI = req.originalUrl.slice(0, 4) === "/api";
     var accessToken = req.query.access_token || req.body.access_token || null;
     if (!isAPI || !accessToken) {
       csrfMiddleware.apply(null, arguments);
@@ -72,26 +72,24 @@ module.exports = function(mongooseConnection, redisClient) {
       next();
     }
   });
-  app.use(function(req, res, next) {
-    if (req.csrfToken)
-      res.locals.csrf_token = req.csrfToken();
+  app.use(function (req, res, next) {
+    if (req.csrfToken) res.locals.csrf_token = req.csrfToken();
     next();
   });
 
   // Pretty print
-  if (app.get('env') === 'development') {
-    app.set('json spaces', 2);
+  if (app.get("env") === "development") {
+    app.set("json spaces", 2);
     // app.locals.pretty = true;
   }
 
   // Append the user to the locals for use in every view
-  app.use(function(req, res, next) {
+  app.use(function (req, res, next) {
     res.locals.user = req.user;
     next();
   });
 
-  require('../config/routes.js')(app, passport);
+  require("../config/routes.js")(app, passport);
 
   app.listen(process.env.PORT || 8080);
-
 };
